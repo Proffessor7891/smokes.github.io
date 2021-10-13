c248fae591df0ba960e83afc654cec348d25c860
Grinevich <proffessor7891@gmail.com>
2021-10-13 14:08:29 +0300

    ok

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+function openForms() {
+    document.getElementById("myForms").style.display = "block";
+  }
+  
+  function closeForms() {
+    document.getElementById("myForms").style.display = "none";
+  }
